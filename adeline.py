a61c6f9c581d2ce25bd4d03604dc40ca239e58ae
Ricardo Ruiz Delgado <rdricardo.96@gmail.com>
2018-09-13 16:03:22 +0200

    normalizacion

--- adeline.py
+++ adeline.py
@@ -1,7 +1,8 @@
-
 import csv
 import pandas as pd
 
+def normalizacion():
+    print("")
 
 with open("DatosPracticaEnergia1.txt") as f:
     reader = csv.reader(f)
@@ -9,19 +10,23 @@ with open("DatosPracticaEnergia1.txt") as f:
 
 # Crea un dataframe con pandas con la información original, con las columnas: counterinfo_id, timestamp y consumption
 txtdata = pd.read_csv('DatosPracticaEnergia1.txt', sep=' ')
-print(txtdata.head())
+# print(txtdata.head())
 # 767
 datos = txtdata.loc[:"V4052"]
 
 print(datos.tail())
 validacion = txtdata.loc["V4053":"V4380"]
-print(validacion.head())
-print(validacion.tail())
-test = txtdata.loc["V4381":]
-print(test.head())
-print(test.tail())
-
-# print(datos.tail())
 # print(validacion.head())
 # print(validacion.tail())
+test = txtdata.loc["V4381":]
 # print(test.head())
+# print(test.tail())
+
+for column in datos:
+
+for column in datos:
+    print(column)
+    datos[column] = (datos[column]-datos[column].min())/(datos[column].max()-datos[column].min())
+
+
+print(datos.head())
